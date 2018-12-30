c58496eef1b67ff2fd9f9dd066e3bd244922e091
RicoBSJ <aubrun.eric@free.fr>
2018-12-30 10:41:04 +0100

    1 Commit Home 30_12

---
+++ src/OrdreAlphabetique.java
@@ -0,0 +1,35 @@
+
+public class OrdreAlphabetique {
+	
+	
+
+	/* Constructeur par défaut
+	 * 
+	 */
+	public OrdreAlphabetique() {
+		
+		return;
+	}
+	
+	/* Constructeur avec paramêtres
+	 * 
+	 */
+	public String OrdreAlphabetique(String classer) {
+		
+		return classer;
+	}
+	
+	public String ordreAlphabetique () {
+		String[] tab = {"z", "c", "b", "a", "r"};
+		return null;
+		/* Résultat attendu : une chaîne de caractère qui est classée par ordre alphabétique a b c r z
+		 * 
+		 */
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
