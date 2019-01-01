a07f0e342ef807eae8a70c05af083dab61ee7ceb
RicoBSJ <aubrun.eric@free.fr>
2019-01-01 17:22:49 +0100

    2 Commit Home 01_01

--- src/OrdreAlphabetique.java
+++ src/OrdreAlphabetique.java
@@ -2,34 +2,36 @@ import java.util.*;
 
 public class OrdreAlphabetique {
 
-	/* Constructeur par défaut
-	 * 
-	 */
-	public OrdreAlphabetique() {
+
+	private String[] tab;
+
+	// Constructeur par défaut
+
+	public OrdreAlphabetique(String[] tab) {
+
+	this.tab = tab;
 		
-		return;
 	}
+	// Résultat attendu : une chaîne de caractère qui est classée par ordre alphabétique a b c r z
 	
-	/* Résultat attendu : une chaîne de caractère qui est classée par ordre alphabétique a b c r z
-	 * 
-	 */
-//	public String ordreAlphabetique () {
-//		String[] tab = {"z", "c", "b", "a", "r"};
-//
-//	}
-	
+	private void ordreAlphabetique() {
+		
+		String[] tab = {"z", "c", "b", "a", "r"};
+		Arrays.sort(tab);
+		
+	}
+
 	public static void main(String[] args) {
 		
-		ArrayList tab = new ArrayList();
-		tab.add('z');
-		tab.add('c');
-		tab.add('b');
-		tab.add('a');
-		tab.add('r');
+		List<String> tab = new ArrayList<String>();
+		tab.add("z");
+		tab.add("c");
+		tab.add("b");
+		tab.add("a");
+		tab.add("r");
 		System.out.println("Dans le désordre : "  +tab);
 		Collections.sort(tab);
 		System.out.println("Dans l'ordre : "  +tab);
-		
 	}
 
 }
