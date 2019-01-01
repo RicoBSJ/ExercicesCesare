20dc1c5673b54f443bdf835c11e97c23264e0989
RicoBSJ <aubrun.eric@free.fr>
2019-01-01 22:26:48 +0100

    3 Commit Home 01_01

--- src/OrdreAlphabetique.java
+++ src/OrdreAlphabetique.java
@@ -2,36 +2,35 @@ import java.util.*;
 
 public class OrdreAlphabetique {
 
-
-	private String[] tab;
-
 	// Constructeur par défaut
 
 	public OrdreAlphabetique(String[] tab) {
 
-	this.tab = tab;
-		
 	}
-	// Résultat attendu : une chaîne de caractère qui est classée par ordre alphabétique a b c r z
-	
+	// Résultat attendu : une chaîne de caractère qui est classée par ordre
+	// alphabétique a b c r z
+
 	private void ordreAlphabetique() {
-		
-		String[] tab = {"z", "c", "b", "a", "r"};
-		Arrays.sort(tab);
-		
+
 	}
 
 	public static void main(String[] args) {
-		
-		List<String> tab = new ArrayList<String>();
-		tab.add("z");
-		tab.add("c");
-		tab.add("b");
-		tab.add("a");
-		tab.add("r");
-		System.out.println("Dans le désordre : "  +tab);
-		Collections.sort(tab);
-		System.out.println("Dans l'ordre : "  +tab);
+
+		String[] tab = { "z", "c", "b", "a", "r" };
+		for (String element : tab) {
+			Collections.sort(element);
+			System.out.println(element);
+		}
 	}
 
 }
+
+// List<String> tab = new ArrayList<String>();
+// tab.add("z");
+// tab.add("c");
+// tab.add("b");
+// tab.add("a");
+// tab.add("r");
+// System.out.println("Dans le désordre : " +tab);
+// Collections.sort(tab);
+// System.out.println("Dans l'ordre : " +tab);
