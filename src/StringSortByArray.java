7385b5517b9a24a79cbb17690e69225b1a90d720
AUBRUNE <AUBRUNE@P520.ari.asso.fr>
2019-01-03 15:13:28 +0100

    4 Commit Job 03_01

---
+++ src/StringSortByArray.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class StringSortByArray {
+
+
+	public StringSortByArray(String[] tab) {
+
+	}
+	// Résultat attendu : une chaîne de caractère qui est classée par ordre
+	// alphabétique a b c r z
+
+	public static void main(String[] args) {
+
+		List<String> tab = new ArrayList<String>();
+		tab.add("z");
+		tab.add("c");
+		tab.add("b");
+		tab.add("a");
+		tab.add("r");
+		System.out.println("Classement d'origine : " + tab);
+		Collections.sort(tab);
+		System.out.println("Classement dans l'ordre : " + tab);
+	}
+}
