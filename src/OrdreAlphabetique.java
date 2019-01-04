313d57966a5174ddd23c3c5cbfae7fd755c0cd1d
RicoBSJ <aubrun.eric@free.fr>
2019-01-04 22:13:45 +0100

    3 Commit Home 04_01

--- src/OrdreAlphabetique.java
+++ src/OrdreAlphabetique.java
@@ -2,17 +2,22 @@ import java.util.*;
 
 public class OrdreAlphabetique {
 
-	// Constructeur par défaut
-
-	public OrdreAlphabetique(String[] tab) {
-
-	}
 	// Résultat attendu : une chaîne de caractère qui est classée par ordre
 	// alphabétique a b c r z
 
 	public static void main(String[] args) {
-
 		String[] tab = { "z", "c", "b", "a", "r" };
+		affiche_tableau(tab);
+		String[] tab2 = new String[4];
+		for (int i = 0; i < tab.length; i++) {
+			tab[i] = tab2[i];
+		}
+	}
 
+	private static void affiche_tableau(String[] tab) {
+		for (int i = 0; i < tab.length; i++) {
+			System.out.print(tab[i]);
+		}
+		System.out.println();
 	}
 }
--- src/StringSortByArray.java
+++ src/StringSortByArray.java
@@ -8,7 +8,7 @@ public class StringSortByArray {
 		System.out.println("List Before: ");
 		for (int i = 0; i < arrays.length; i++) {
 			System.out.print(arrays[i] + ",");
-		}
+		} 
 		System.out.println("\rList After: ");
 		Arrays.sort(arrays);
 
