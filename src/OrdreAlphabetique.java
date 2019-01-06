97194f1c6f26b804f3c0ce62b6964788fb41a5c4
RicoBSJ <aubrun.eric@free.fr>
2019-01-06 01:39:11 +0100

    1 Commit Home 06_01

--- src/OrdreAlphabetique.java
+++ src/OrdreAlphabetique.java
@@ -8,9 +8,16 @@ public class OrdreAlphabetique {
 	public static void main(String[] args) {
 		String[] tab = { "z", "c", "b", "a", "r" };
 		affiche_tableau(tab);
-		String[] tab2 = new String[4];
+//		String[] tab2 = new String[5];
+		int j = 0;
 		for (int i = 0; i < tab.length; i++) {
-			tab[i] = tab2[i];
+			tab[i] = tab[4];
+			if (i > j) {
+				tab[i] = tab[4];
+			} else {
+				tab[i] = tab[0];
+			}
+			System.out.println(i);
 		}
 	}
 
