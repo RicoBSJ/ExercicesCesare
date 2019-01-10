0019950d7b7d2b0fa1735a0811f41e17e9607382
RicoBSJ <aubrun.eric@free.fr>
2019-01-11 00:40:25 +0100

    1 Commit Home 11_01

--- src/OrdreAlpha1.java
+++ src/OrdreAlpha1.java
@@ -1,31 +1,16 @@
-import java.util.Arrays;
-import java.util.Scanner;
 
 public class OrdreAlpha1 {
 
 	public static void main(String[] args) {
 		// Résultat attendu : une chaîne de caractère qui est classée par ordre
 		// alphabétique a b c r z
-
 		String[] tab = { "z", "c", "b", "a", "r" };
 		affiche_tableau(tab);
 
-		int[] resultat = { 0, 0, 0, 0 };
-		System.out.println("Veuillez taper 4 chiffres :");
-		Scanner sc = new Scanner(System.in);
-		String result = sc.nextLine();
-		for (int i = 0; i < resultat.length; i++) {
-			String carac = Character.toString(result.charAt(i));
-			resultat[i] = Integer.parseInt(carac);
+		for (int i = 0; i < tab.length; i++) {
+			char c = tab[i].charAt(0);
+			System.out.println(c);
 		}
-
-		// for (int i = 0; i < tab.length; i++) {
-		//
-		// tab[i] = tab[4];
-		//
-		// System.out.println(i);
-		//
-		// }
 	}
 
 	private static void affiche_tableau(String[] tab) {
@@ -34,4 +19,5 @@ public class OrdreAlpha1 {
 		}
 		System.out.println();
 	}
+
 }
