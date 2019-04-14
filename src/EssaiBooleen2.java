926ae795329dba2790b4c4708b3aa8653fe2517c
RicoBSJ <aubrun.eric@free.fr>
2019-04-14 22:23:23 +0200

    1 Commit Home 14_04

---
+++ src/EssaiBooleen2.java
@@ -0,0 +1,41 @@
+
+
+import com.Constante;
+
+public class EssaiBooleen2 {
+
+	public static void main(String[] args) {
+
+//		boolean reponse = false;
+//		String str = String.valueOf(reponse);
+//		String strArray[] = str.split(" ");
+//		System.out.println("String converted to String array");
+//		for (int i = 0; i < strArray.length; i++) {
+//			System.out.println(strArray[i]);
+//		}
+
+//		char[] array = str.toCharArray();
+//		char[] charArray = str.toCharArray();
+//		Character[] charObjectArray = ArrayUtils.toObject(charArray);
+//		String[] tabResultat = new String[Constante.tabColor.length];
+//		for (char output : array) {
+//			System.out.println(output);
+//		}
+
+		String[] tabEssai = new String[7];
+		tabEssai = Constante.tabColor;
+		String[] tabResultat = { "vert", "orange", "jaune", "gris", "noir", "rouge", "bleu" };
+		boolean reponse = false;
+		String str = String.valueOf(reponse);
+		String strArray[] = str.split(" ");
+		for (int i = 0; i < tabEssai.length; i++) {
+			if (strArray[i] == "true") {
+				tabResultat[i] = tabEssai[i];
+				if (strArray[i] == "false") {
+					tabResultat[i] = tabEssai[i + 1];
+				}
+				System.out.println(strArray[i]);
+			}
+		}
+	}
+}
---
+++ src/ResultatGagnant.java
@@ -0,0 +1,16 @@
+
+
+import com.Constante;
+
+public class ResultatGagnant {
+
+	public static void main(String[] args) {
+		
+		String resultatGagnantColor = "";
+		for (int i = 0; i < Constante.longueurCombinaison; i++) {
+			resultatGagnantColor += "\rLa couleur à l'emplacement " + (i + 1) + " est identique ";
+		}
+		System.out.println(resultatGagnantColor);
+	}
+
+}
