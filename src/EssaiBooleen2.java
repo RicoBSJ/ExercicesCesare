870ad5350bc6686b92e8c236ad2f62ec48026535
RicoBSJ <aubrun.eric@free.fr>
2019-04-15 15:38:08 +0200

    1 Commit Home 15_04

--- src/EssaiBooleen2.java
+++ src/EssaiBooleen2.java
@@ -3,22 +3,6 @@ public class EssaiBooleen2 {
 
 	public static void main(String[] args) {
 
-//		boolean reponse = false;
-//		String str = String.valueOf(reponse);
-//		String strArray[] = str.split(" ");
-//		System.out.println("String converted to String array");
-//		for (int i = 0; i < strArray.length; i++) {
-//			System.out.println(strArray[i]);
-//		}
-
-//		char[] array = str.toCharArray();
-//		char[] charArray = str.toCharArray();
-//		Character[] charObjectArray = ArrayUtils.toObject(charArray);
-//		String[] tabResultat = new String[Constante.tabColor.length];
-//		for (char output : array) {
-//			System.out.println(output);
-//		}
-
 		String[] tabEssai = { "vert", "orange", "jaune", "gris", "noir", "rouge", "bleu" };
 		String[] tabResultat = { "vert", "orange", "jaune", "gris", "noir", "rouge", "bleu" };
 		boolean reponse = false;
@@ -30,7 +14,7 @@ public class EssaiBooleen2 {
 				if (strArray[i] == "false") {
 					tabResultat[i] = tabEssai[i + 1];
 				}
-				System.out.println(strArray[i]);
+				System.out.println(tabResultat[i]);
 			}
 		}
 	}
