cb3bd771209e19effac550017d1e2a4f6938e094
AUBRUNE <AUBRUNE@P520.ari.asso.fr>
2019-05-03 10:34:03 +0200

    2 Commit Job 03_05

--- src/Decaler.java
+++ src/Decaler.java
@@ -21,5 +21,7 @@ public class Decaler {
 		int[] unOrderedArr = { 1, 2, 3, 4, 5, 6, 7, 8 };
 		int orderToRotate = 1;
 		decaler(unOrderedArr, orderToRotate);
+		System.out.println();
+		decaler(unOrderedArr, orderToRotate);
 	}
 }
